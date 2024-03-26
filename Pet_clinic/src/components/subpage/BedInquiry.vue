63666452c6b77aec881e53118af57a599167fbbd
Cipher <3351897582@qq.com>
2024-03-26 22:22:36 +0800

    modify BedInquiry and FunctionalStudy

--- Pet_clinic/src/components/subpage/BedInquiry.vue
+++ Pet_clinic/src/components/subpage/BedInquiry.vue
@@ -1,23 +1,88 @@
 <template>
-  <div class="demo-image">
-    <div v-for="fit in fits" :key="fit" class="block">
-      <span class="demonstration">{{ fit }}</span>
-      <el-image style="width: 100px; height: 100px" :src="url" :fit="fit" />
-    </div>
-  </div>
+  <el-container style="margin-top: 2vw;">
+    <el-aside style="margin-left: 5vw; z-index: 99;">
+      <el-select v-model="value" placeholder="Select" style="margin-top: 3vw;">
+        <el-option v-for="dep in departments" :key="dep.departmentId" :label="dep.name" :value="dep.name" 
+         @click="selectDepartment(dep.departmentId)"/>
+      </el-select>
+      <el-table :data="bedList" style="width:100%; margin-top: 3vw;">
+        <el-table-column prop="bedId" label="床位号" width="50%" />
+        <el-table-column prop="location" label="位置" width="50%" />
+      </el-table>
+    </el-aside>
+    <el-main style="margin-left: 10vw; margin-top: 2vw;">
+      <div class="bed-image">
+          <el-image style="width: 45vw; height: 25vw;" :src="url"/>
+      </div>
+    </el-main>
+  </el-container>
+
+
+
 </template>
 
 <script setup lang="ts">
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
+import { ref } from 'vue'
 
 defineComponent({
   name: "BedInquiry"
 })
-const fits = ['fill', 'contain']
+
+function selectDepartment(id: string) {
+  var temp = departments.find(dep => dep.departmentId === id);
+  if (temp != null) currentDepartment = temp;
+  console.log(currentDepartment.departmentId);
+}
+
+const value = ref('')
+var departments = [
+  {
+    departmentId: '001',
+    name: '部门1',
+    desc: '略'
+  },
+  {
+    departmentId: '002',
+    name: '部门2',
+    desc: '略'
+  },
+  {
+    departmentId: '003',
+    name: '部门3',
+    desc: '略'
+  }
+]
+var currentDepartment = {
+  departmentId: '001',
+  name: '部门1',
+  desc: '略'
+}
+var bedList = [
+  {
+    bedId: '001001',
+    departmentId: '001',
+    location: '001A'
+  },
+  {
+    bedId: '001002',
+    departmentId: '001',
+    location: '001B'
+  },
+  {
+    bedId: '001003',
+    departmentId: '001',
+    location: '001C'
+  }
+]
+
+const fits = ['fill']
 const url =
-    'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+  'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
 </script>
 
 <style scoped lang="scss">
-
+// .el-table__body, .el-table__header{
+//     width: 100%;
+//   }
 </style>
--- Pet_clinic/src/components/subpage/FunctionalStudy.vue
+++ Pet_clinic/src/components/subpage/FunctionalStudy.vue
@@ -1,23 +1,47 @@
 <template>
-  <div class="demo-image">
-    <div v-for="fit in fits" :key="fit" class="block">
-      <span class="demonstration">{{ fit }}</span>
-      <el-image style="width: 100px; height: 100px" :src="url" :fit="fit" />
-    </div>
-  </div>
+  <el-row :gutter="80">
+    <el-col :span="8">
+      <el-result title="角色扮演" sub-title="可选前台、医助、医师三种角色">
+        <template #icon>
+          <el-image src="url" />
+        </template>
+        <template #extra>
+          <el-button type="primary">进入</el-button>
+        </template>
+      </el-result>
+    </el-col>
+    <el-col :span="8">
+      <el-result title="病例学习" sub-title="目录查找学习病种知识">
+        <template #icon>
+          <el-image src="url" />
+        </template>
+        <template #extra>
+          <el-button type="primary">进入</el-button>
+        </template>
+      </el-result>
+    </el-col>
+    <el-col :span="8">
+      <el-result title="在线测试" sub-title="选择套题或单题进行测试">
+        <template #icon>
+          <el-image src="url" />
+        </template>
+        <template #extra>
+          <el-button type="primary">进入</el-button>
+        </template>
+      </el-result>
+    </el-col>
+  </el-row>
 </template>
 
 <script setup lang="ts">
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 
 defineComponent({
   name: "FunctionalStudy"
 })
 const fits = ['fill']
 const url =
-    'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+  'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
