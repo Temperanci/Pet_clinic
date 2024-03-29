ee01984b97fbef4bb5582777296839db1f6ba8ee
HLI5KY <husky_yooo@163.com>
2024-03-29 14:24:01 +0800

    实现后台功能页面切换

--- Pet_clinic/src/components/BackgroundLayout.vue
+++ Pet_clinic/src/components/BackgroundLayout.vue
@@ -19,12 +19,14 @@
         </div>
       </el-card>
     </el-header>
+
   <el-container class="mainPage">
     <div class="aside" v-if="aside">
       <div class="console">
-        <div style="width: 100%;">
-        aside
-      </div>
+          <div class="consoleBtn" v-for="guidance in guidanceMap" 
+          :key="guidance[0]" >
+              <p @click="handleSelect(guidance[0])">{{ guidance[1] }}</p>
+          </div>
       </div>
     </div>
       <div style="align-items:center;display: flex;width:0%">
@@ -33,8 +35,12 @@
             <el-icon v-else><ArrowLeft /></el-icon>
       </span>
       </div>
-    <div class="main"></div>
-
+      
+    <div class="main">
+      <component :is="currentComponent">
+      
+      </component>
+    </div>
   </el-container>
   
   </el-container>
@@ -42,16 +48,53 @@
 <script setup lang="ts">
 import { defineComponent,ref} from 'vue'
 import type {DefineComponent} from 'vue'
-
+import BedManagement from './background/BedManagement.vue'
+import CaseManagement from './background/CaseManagement.vue'
+import ChargeManagement from './background/ChargeManagement.vue'
+import DepartmentManagement from './background/DepartmentManagement.vue'
+import DrugManagement from './background/DrugManagement.vue'
+import StaffManagement from './background/StaffManagement.vue'
+import StudyManagement from './background/StudyManagement.vue'
+import CreateTest from './background/CreateTest.vue'
+import QuestionBank from './background/QuestionBank.vue'
+import FinishedTest from './background/FinishedTest.vue'
 defineComponent({
   name: "BackgroundLayout"
 })
 
-const aside = ref(false);//控制侧栏的可见性
+const aside = ref(true);//控制侧栏的可见性
 const emit = defineEmits(['switch'])
 function Switch(n:number){
     emit('switch',n)
 }//发送消息到APP.vue
+
+interface ComponentsMap {
+  [key: string]: DefineComponent<{}, {}, any>;
+}
+const componentsMap: ComponentsMap = {
+  '1':BedManagement,
+  '2':CaseManagement,
+  '3':ChargeManagement,
+  '4':DepartmentManagement,
+  '5':DrugManagement,
+  '6':StaffManagement,
+  '7':StudyManagement
+};
+const guidanceMap = new Map([
+  ['1','住院管理'],
+  ['2','病例管理'],
+  ['3','收费管理'],
+  ['4','科室管理'],
+  ['5','药品管理'],
+  ['6','人员管理'],
+  ['7','学习管理']
+])//导航名称
+function handleSelect(n:string){
+  activeIndex.value = n;
+  currentComponent.value = componentsMap[activeIndex.value];
+}
+const activeIndex = ref('1')
+const currentComponent = ref(componentsMap['1']);
 </script>
 <style scoped lang="scss">
 .el-container {
@@ -66,6 +109,7 @@ function Switch(n:number){
   border-width: 2px;
   border-left-style: hidden;
   align-items:center;display: flex;
+  z-index: 0;
 }
 .mainPage {
   margin-top: 3%;
@@ -124,7 +168,18 @@ function Switch(n:number){
     border-right-width: 2px;
     width: 20%;
 }
+.asideBar{
+      display: flex;
+      width: auto;
+      height: 100%;
+      .div{
+        width: 100%;
+        height: 3%;
+      }
+    }
 .main {
+    height: 100%;
+    width: 100%;
     text-align: center;
     background: rgb(244, 244, 245);
 }
@@ -137,10 +192,19 @@ function Switch(n:number){
   background-color: #ece2e2;
   color: #909399;
 }
+.consoleBtn{
+  height: auto;
+  width: 100%;
+  border-color: #DCDFE6;
+  border-style: solid;
+  border-width: 1px;
+
+}
 .console {
   height: 100%;
   width: 100%;
   display: flex;
+  flex-direction: column;
 }
 
 </style>
---
+++ Pet_clinic/src/components/background/DepartmentManagement.vue
@@ -0,0 +1,23 @@
+<template>
+<p>
+  {{ component.name }}
+</p>
+  
+  </template>
+  
+  <script setup lang="ts">
+  import { defineComponent } from "vue";
+  import { ref } from 'vue'
+  
+  const component = defineComponent({
+    name: "DepartmentManagement"
+  })
+  
+  </script>
+  
+  <style scoped lang="scss">
+  // .el-table__body, .el-table__header{
+  //     width: 100%;
+  //   }
+  </style>
+  
