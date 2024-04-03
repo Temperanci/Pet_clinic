e21763f35b848aef7ee2c5faa6a3a18003741f0f
Cipher <78262790+Cipher0xf@users.noreply.github.com>
2024-04-03 12:56:07 +0800

    修改了测试页面 (#4)
    * modify BedInquiry and FunctionalStudy
    
    * modify testpage
    
    * modify testpage

--- Pet_clinic/src/components/subpage/OnlineTestComponent.vue
+++ Pet_clinic/src/components/subpage/OnlineTestComponent.vue
@@ -1,16 +1,76 @@
 <template>
   <div class="online-test-component">
-    <el-button class="back-button" @click="close">返回</el-button>
+    <el-button class="back-button" @click="returnUpper">返回上一级</el-button>
     <!-- 在这里添加在线测试相关的内容 -->
-  </div>
+    <div class="test-content">
+      <template v-if="content === 0">
+        <div class="testType">
+          <el-row :gutter="80">
+            <el-col :span="12">
+              <el-result title="套题" sub-title="选择试卷参加测试">
+                <template #icon>
+                  <el-image src="url" />
+                </template>
+                <template #extra>
+                  <el-button type="primary" @click="changeContent(2)">进入</el-button>
+                </template>
+              </el-result>
+            </el-col>
+            <el-col :span="12">
+              <el-result title="单题" sub-title="按知识点筛选相关题目">
+                <template #icon>
+                  <el-image src="url" />
+                </template>
+                <template #extra>
+                  <el-button type="primary" @click="changeContent(1)">进入</el-button>
+                </template>
+              </el-result>
+            </el-col>
+          </el-row>
+        </div>
+      </template>
+      <SingleProblem v-if="content === 1" @page="(n: number) => content = n" />
+      <ProblemSet v-if="content === 2" @page="(n: number) => content = n" />
+      <Test v-if="content === 3" @page="(n: number) => content = n" />
+    </div>
+  </div> 
 </template>
 
 <script setup lang="ts">
-import {defineComponent} from "vue";
+import { defineComponent, ref } from "vue";
+import ProblemSet from '../testpage/ProblemSet.vue';
+import SingleProblem from '../testpage/SingleProblem.vue';
+import Test from "../testpage/Test.vue";
 
 defineComponent({
-  name: "OnlineTestComponent"
+  name: "OnlineTestComponent",
+  components: {
+    SingleProblem,
+    ProblemSet,
+    Test
+  }
 })
+var content = ref(0)
+
+function changeContent(n: number) { //改变测试页面内容
+  content.value = n;
+  console.log('changeContent: ', content.value)
+}
+function returnUpper() {
+  switch (content.value) {
+    case 1:
+      changeContent(0);
+      break;
+    case 2:
+      changeContent(0);
+      break;
+    case 3:
+      changeContent(2);
+      break;
+    default:
+      changeContent(0);
+  }
+}
 
 const close = defineEmits(['close'])
 </script>
@@ -25,5 +85,9 @@ const close = defineEmits(['close'])
   position: absolute;
   top: 10px;
   left: 10px;
+}
+.test-content{
+  position: relative;
+  top:50px;
 }
 </style>
---
+++ Pet_clinic/src/components/testpage/SingleProblem.vue
@@ -0,0 +1,52 @@
+<template>
+    <el-container>
+        <el-main>
+
+        </el-main>
+    </el-container>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from "vue";
+import { ref } from 'vue'
+
+defineComponent({
+    name: "SingleProblem",
+})
+
+
+
+const ProblemSetList = [{
+    problemSetId: '001',
+    title: '测试1',
+    desc: '略',
+    startTime: '',
+    endTime: '',
+    duration: '',
+    whiteList: '',
+    problemIdList: []
+}, {
+    problemSetId: '002',
+    title: '测试2',
+    desc: '略',
+    startTime: '',
+    endTime: '',
+    duration: '',
+    whiteList: '',
+    problemIdList: []
+}, {
+    problemSetId: '003',
+    title: '测试3',
+    desc: '略',
+    startTime: '',
+    endTime: '',
+    duration: '',
+    whiteList: '',
+    problemIdList: []
+}
+]
+
+const url = ''
+</script>
+
+<style scoped lang="scss"></style>
