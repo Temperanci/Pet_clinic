9a8bdb7cdff3db59a07b6d5884c32f941f03cf38
Cipher <3351897582@qq.com>
2024-04-17 00:13:34 +0800

    modify Test

--- Pet_clinic/src/components/testpage/Test.vue
+++ Pet_clinic/src/components/testpage/Test.vue
@@ -3,12 +3,14 @@
         <el-container>
             <el-container width="60%">
                 <el-header>
-                    <h3>题{{ selectedIndex + 1 }}:{{ selectedProblem.problem.title }}</h3>
+                    <h3>题{{ selectedIndex + 1 }}:（{{ selectedProblem.score }}分）</h3>
+                    <h4>{{ selectedProblem.problem.title }}</h4>
                 </el-header>
                 <el-main>
                     <div class="problem-content">
                         <pre style="font-size: larger;">{{ selectedProblem.problem.content }}</pre>
-                        <el-radio-group v-if="selectedProblem.problem.type === 'objective'" v-model="answer[selectedIndex]">
+                        <el-radio-group v-if="selectedProblem.problem.type === 'objective'"
+                            v-model="answer[selectedIndex]">
                             <el-radio v-for="(choice, index) in choices" :key="index" :label="choice">
                                 {{ choice }}
                             </el-radio>
@@ -18,8 +20,8 @@
                                 {{ choice }}
                             </el-checkbox>
                         </el-checkbox-group> -->
-                        <el-input v-else-if="selectedProblem.problem.type === 'subjective'" type="textarea" placeholder="在此输入答案"
-                            v-model="answer[selectedIndex]" />
+                        <el-input v-else-if="selectedProblem.problem.type === 'subjective'" type="textarea"
+                            placeholder="在此输入答案" v-model="answer[selectedIndex]" />
                     </div>
                 </el-main>
                 <el-footer>
@@ -31,6 +33,13 @@
                 </el-footer>
             </el-container>
             <el-aside>
+
+                <div class="countdown">
+                    剩余时间:
+                    {{ Math.floor(clock / 3600000) }}h {{ Math.floor((clock % 3600000) / 60000) }}min {{
+                        Math.floor((clock % 60000) / 1000) }}s
+                </div>
+
                 <div class="problem-table">
                     <div class="problem-number" v-for="(pro, index) in problemList">
                         <el-button style="width: 45px" @click="jumpProblem(index)" :style="{
@@ -41,17 +50,17 @@
                     </div>
                 </div>
                 <div class="test-button">
-                    <el-button @click="submitDialog = true">提交</el-button>
+                    <el-button @click="submitDialog = true; checkAnswerMap();">提交</el-button>
                 </div>
             </el-aside>
         </el-container>
     </div>
 
     <el-dialog v-model="submitDialog" title="" width="400">
-        <span>确认提交试卷？</span>
+        <span>{{ warningMsg }}</span>
         <template #footer>
             <div class="dialog-footer">
-                <el-button @click="submitDialog = false">取消</el-button>
+                <el-button v-if="clock !== 0" @click="submitDialog = false">取消</el-button>
                 <el-button type="primary" @click="submitDialog = false; submit()">
                     提交
                 </el-button>
@@ -81,11 +90,14 @@ const props = defineProps({
     enterTime: Date
 });
 
+const warningMsg = ref('确认提交试卷？');
 const choices = ref(['A', 'B', 'C', 'D']);
+const clock = ref(999999);
 const answer = ref<string[]>([]); //暂存答案
 const answerMap = new Map<string, string>(); //题号id-->考生答案
 
-var selectedIndex = ref(0);
+
+const selectedIndex = ref(0);
 const selectedProblem = ref<{ problem: ProblemBO, score: number }>({
     problem: {
         type: '',
@@ -108,19 +120,23 @@ async function fetchProblems() {
         const problemSetResponse = await problemSetQuery(problemSetRequest);
         const idList = problemSetResponse.data.datas[0].problemIdList;
         const scoreList = problemSetResponse.data.datas[0].problemScoreMap;
-        console.log('获取测试:',problemSetResponse.data.datas[0]);
-        idList.forEach(async (id:string) => {
-            const problemRequest: ProblemPageRequest = {problemId: id};
+        clock.value = problemSetResponse.data.datas[0].duration;
+        clock.value = 2000;
+        console.log('获取测试:', problemSetResponse.data.datas[0]);
+        idList.forEach(async (id: string) => {
+            const problemRequest: ProblemPageRequest = { problemId: id };
             const problemResponse = await problemQuery(problemRequest);
             var temp = {
-                problem:problemResponse.data.datas[0],
-                score:scoreList[id]
+                problem: problemResponse.data.datas[0],
+                score: scoreList[id]
             };
             temp.problem.content = (temp.problem.content ?? '').replace(/(A\.|B\.|C\.|D\.)/g, '\n$1'); //单选题选项换行
-            console.log('获取题目:',temp);
+            console.log('获取题目:', temp);
             problemList.value.push(temp);
         })
         console.log("获取problemList:", problemList.value);
+        setTimeout(() => { jumpProblem(selectedIndex.value); }, 100);
+
     } catch (error) {
         console.error('Error fetching problems:', error);
     }
@@ -152,6 +168,17 @@ onMounted(async () => {
     await fetchProblems();
 })
 
+//考试倒计时
+const count = setInterval(() => {
+    if (clock.value >= 1000) {
+        clock.value -= 1000;
+    } else if (clock.value == 0) {
+        clearInterval(count);
+        warningMsg.value='考试时间已结束！';
+        submitDialog.value = true;
+    }
+}, 1000);
+
 //题目跳转
 function jumpProblem(i: number) {
     var pro = problemList.value[i];
@@ -160,17 +187,15 @@ function jumpProblem(i: number) {
         console.log(answer.value[i]);
         selectedProblem.value = pro;
         selectedIndex.value = i;
-        console.log('跳转至题', i + 1, ':', selectedProblem.value.problem.title);
+        // console.log('跳转至题', i + 1, ':', selectedProblem.value.problem.title);
     } else {
         console.log('跳转失败，题目不存在！');
     }
 }
 function nextProblem() {
-    saveAnswer();
     jumpProblem(selectedIndex.value + 1);
 }
 function priorProblem() {
-    saveAnswer();
     jumpProblem(selectedIndex.value - 1);
 }
 
@@ -179,23 +204,28 @@ function saveAnswer() {
     var pro = selectedProblem.value;
     var temp = answer.value[selectedIndex.value];
     if (pro.problem.type == 'objective') {
-        console.log("单选:", temp);
+        // console.log("单选:", temp);
         answerMap.set(pro.problem.problemId ?? '', temp);
     } else if (pro.problem.type == 'subjective') {
-        console.log("简答:", temp);
+        // console.log("简答:", temp);
         answerMap.set(pro.problem.problemId ?? '', temp);
     }
+}
+
+//检查是否已完成全部题目
+function checkAnswerMap() {
+    saveAnswer();
+    problemList.value.forEach(pro => {
+        if (pro.problem.problemId && !answerMap.has(pro.problem.problemId)) {
+            console.log('未答题:', problemList.value.indexOf(pro));
+            warningMsg.value = '有题目未作答，确认提交？';
+        }
+    })
 }
 
 //提交试卷
 function submit() {
     saveAnswer();
-    // answer.value.forEach(ans => {
-    //     console.log(ans);
-    // });
-    // if (answer.value.some(ans => ans === undefined || ans === '')) {
-    //     console.log('Array answer contains undefined or empty strings');
-    // }
     const testRecord: TestRecordBO = {
         problemSetId: '',
         candidateId: '',
@@ -216,7 +246,7 @@ function submit() {
     // console.log("提交测试记录:", temp);
     emit('content', 'ProblemSet'); //返回试卷列表
 }
-const submitDialog = ref(false)
+var submitDialog = ref(false)
 
 </script>
 
